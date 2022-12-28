3473999aabd92cf41b485d01a4b2401582ff53f2
Kushagra Soni <kushagra.soni@outlook.com>
2022-12-28 17:20:16 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to run Learn React
         </a>
       </header>
     </div>
